7ecc31e3263bdf8ff5d9a1fe1ab909c51277c79c
Tonio Liebrand <andreas.liebrand@hof.uni-frankfurt.de>
2019-06-30 11:20:27 +0200

    Refactoring

--- DragableFunctions.R
+++ DragableFunctions.R
@@ -3,13 +3,22 @@ library(shiny)
 dataSelect <- reactiveValues(type = "all")
 
 # To be called from ui.R
-DragableChartOutput <- function(inputId, width="500px", height="500px") {
-  style <- sprintf("width: %s; height: %s;",
-    validateCssUnit(width), validateCssUnit(height))
+DragableChartOutput <- function(inputId, width = "500px", height = "500px") {
+  style <- sprintf(
+    fmt = "width: %s; height: %s;",
+    validateCssUnit(width), 
+    validateCssUnit(height)
+  )
+  
   tagList(
     tags$script(src = "d3.v3.min.js"),
+    
     includeScript("ChartRendering.js"),
-    div(id=inputId, class="Dragable", style = style,
+    
+    div(
+      id = inputId, 
+      class = "Dragable", 
+      style = style,
       tag("svg", list())
     )
   )
@@ -17,9 +26,24 @@ DragableChartOutput <- function(inputId, width="500px", height="500px") {
 
 # To be called from server.R
 renderDragableChart <- function(expr, env = parent.frame(), quoted = FALSE, color = "orange", r = 10) {
-  installExprFunction(expr, "data", env, quoted)
+  installExprFunction(
+    expr = expr, 
+    name = "data", 
+    eval.env = env, 
+    quoted = quoted
+  )
+  
   function(){
-    data <- lapply(1:dim(data())[1], function(idx) list(x = data()$x[idx], y = data()$y[idx], r = r))
-    list(data = data, col = color)
+    data <- lapply(
+      X = 1:dim(data())[1], 
+      FUN = function(idx){
+        list(x = data()$x[idx], y = data()$y[idx], r = r)    
+      }
+    )
+    
+    list(
+      data = data, 
+      col = color
+    )
   } 
-}
+}R
